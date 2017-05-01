bead21c5bb864aa28e52adec8b7d522873da81e6
Brandon Conway <brandoncc@gmail.com>
2017-05-01 15:06:53 -0700

    Remove unused variables

--- extension.js
+++ extension.js
@@ -1,10 +1,12 @@
-const { commands, window, workspace } = require('vscode');
+const { commands } = require('vscode');
 const Categories = require('./lib/categories');
 
 function activate(context) {
   const categories = new Categories();
 
   var disposable = commands.registerCommand('railsProjectNavigator.navigate', function () {
+    // The return value is wrapped in a new promise so that we can gracefully
+    // handle "no file selected" and "no category selected" errors"
     return new Promise((res, rej) => {
       categories.showCategories()
         .then(categories.getFilesFor.bind(categories), rej)
